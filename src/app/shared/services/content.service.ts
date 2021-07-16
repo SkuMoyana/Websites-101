a99ea28c7c7204ce0323be8c9102314a3576bed0
Sikululekile Moyana <ext.siku.moyana@veneka.com>
2021-07-16 22:15:48 +0200

    main css

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -7,7 +7,7 @@ export class ContentService {
 
   constructor() { }
   pages: any = {
-    'home': {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', image: 'assets/bg00.jpg'},
+    'home': {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', image: 'assets/bg00.jpg', services : ['service1','service2','service3']},
     'about': {title: 'About', subtitle: 'About Us', content: 'Some content about us.', image: 'assets/bg01.jpg'},
     'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'How to contact us.', image: 'assets/bg02.jpg'}
   };
